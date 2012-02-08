a1dad92206e5368cb4707f192408815fdcb0706d
Mathias Geat <mathias@ailoo.net>
2012-02-08 19:47:20 +0100

    remove old dummy files

--- skin/frontend/boomstrap/default/css/screen.css
+++
@@ -1,8 +0,0 @@
-/**
- * Boomstrap
- *
- * @category    design
- * @package     boomstrap
- * @copyright   Copyright (c) 2012 Holzweg e-commerce solutions (http://www.holzweg.com)
- * @license     http://www.apache.org/licenses/LICENSE-2.0  Apache License v2.0
- */
