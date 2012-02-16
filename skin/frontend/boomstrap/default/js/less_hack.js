f46ba667d0a13beae01c11eed8202e3a210f0f86
Florian Peter <florian.peter@gmx.at>
2012-02-17 00:50:42 +0100

    Add a second less.js hack, muchachos.
    Against crazy local storage caching, --> see
    http://stackoverflow.com/questions/7191619/why-does-an-imported-css-file-get-stored-in-localstorage-and-not-refresh-like-a

--- skin/frontend/boomstrap/default/js/less_hack.js
+++ skin/frontend/boomstrap/default/js/less_hack.js
@@ -1,1 +1,20 @@
 (less = less || {}).env = 'development';
+
+// Destroys the localStorage copy of CSS that less.js creates
+function destroyLessCache(pathToCss) { // e.g. '/css/' or '/stylesheets/'
+
+  if (!window.localStorage || !less || less.env !== 'development') {
+    return;
+  }
+  var host = window.location.host;
+  var protocol = window.location.protocol;
+  var keyPrefix = protocol + '//' + host + pathToCss;
+  
+  for (var key in window.localStorage) {
+    if (key.indexOf(keyPrefix) === 0) {
+      delete window.localStorage[key];
+    }
+  }
+}
+
+destroyLessCache('/skin/frontend/boomstrap/default/less/');
